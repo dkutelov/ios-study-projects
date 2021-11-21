3067752f21f4758d72a4fc40cd835207f4404e39
dkutelov <dkutelov@digitals.bg>
2021-11-21 22:57:44 +0200

    location screens, partially route screens

---
+++ LyftProject/LyftProject/VControllers/RouteSCreen/RideQuoteCell.swift
@@ -0,0 +1,23 @@
+//
+//  RideQuoteCell.swift
+//  LyftProject
+//
+//  Created by Dariy Kutelov on 21.11.21.
+//
+
+import UIKit
+
+class RideQuoteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
