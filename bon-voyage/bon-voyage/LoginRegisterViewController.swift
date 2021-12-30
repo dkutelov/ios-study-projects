88d81ed86fb2bfbde4083f3d46a4c0adeaecd4c1
Dari <dari.k@abv.bg>
2021-12-30 17:23:29 +0200

    Bon Voyage - initial, login and register ui

---
+++ bon-voyage/bon-voyage/LoginRegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginRegisterViewController.swift
+//  Bon-voyage
+//
+//  Created by Dari Kutelov on 30.12.21.
+//
+
+import UIKit
+
+class LoginRegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
