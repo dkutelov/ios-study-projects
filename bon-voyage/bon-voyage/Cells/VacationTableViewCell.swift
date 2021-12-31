7262247ae41d763191f2ab3afbebfc1cacbda1c5
dkutelov <dkutelov@digitals.bg>
2021-12-31 12:07:14 +0200

    Vacation table view and table view cell

--- bon-voyage/bon-voyage/Cells/VacationTableViewCell.swift
+++ bon-voyage/bon-voyage/Cells/VacationTableViewCell.swift
@@ -6,18 +6,44 @@
 //
 
 import UIKit
+import SDWebImage
 
 class VacationTableViewCell: UITableViewCell {
-
+    
+    //Mark: - IBOutlets
+    
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        //Cell content view
+        layer.shadowColor = UIColor.darkGray.cgColor
+        layer.shadowRadius = 3.0
+        layer.shadowOpacity = 1.0
+        layer.shadowOffset = CGSize.zero
+        
+        // Card UIView
+        cardView.layer.cornerRadius = 10
+        cardView.layer.masksToBounds = true
+        cardView.layer.borderWidth = 0.5
+        cardView.layer.borderColor = UIColor(named: "border_blue")?.cgColor
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+    func configureCell(vacation: Vacation) {
+        titleLabel.text = vacation.title
+        priceLabel.text = "\(vacation.price)"
+        
+        if let imageUrl = vacation.images.first,
+        let url = URL(string: imageUrl) {
+            mainImage.sd_imageIndicator = SDWebImageActivityIndicator.medium //spinner when loading
+            mainImage.sd_setImage(with: url, placeholderImage: UIImage(named: "background-beach-alpha"))
+        }
     }
     
 }
--- bon-voyage/bon-voyage/Controllers/HomeViewController.swift
+++ bon-voyage/bon-voyage/Controllers/HomeViewController.swift
@@ -8,16 +8,40 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    var vacations = [Vacation]()
 
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let loginRegisterViewController = LoginRegisterViewController()
-        loginRegisterViewController.modalPresentationStyle = .fullScreen
-        present(loginRegisterViewController, animated: true)
+        
+        title = "Vacation Packages"
+        vacations = demoData
+        setupTableView()
+        
+//        let loginRegisterViewController = LoginRegisterViewController()
+//        loginRegisterViewController.modalPresentationStyle = .fullScreen
+//        present(loginRegisterViewController, animated: true)
     }
     
-
+    // MARK: - Private Methods
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView() //gets rid of emtry cells at the end of the table
+        tableView.contentInset.top = 8 // space above first cell
+        tableView.register(UINib(nibName: "VacationTableViewCell", bundle: nil), forCellReuseIdentifier: "VacationCell")
+    }
+    
+    
+    // MARK: - IBActions
+    
     @IBAction func userIconDidTapped(_sender: Any) {
         
         let userSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -45,5 +69,29 @@ class HomeViewController: UIViewController {
         
         present(userSheet, animated: true)
     }
+}
 
+// MARK: Table View Datasource
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vacations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "VacationCell", for: indexPath) as! VacationTableViewCell
+        cell.configureCell(vacation: vacations[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 220
+    }
+}
+
+// MARK: Table View Datasource
+
+extension HomeViewController: UITableViewDelegate {
+    
 }
