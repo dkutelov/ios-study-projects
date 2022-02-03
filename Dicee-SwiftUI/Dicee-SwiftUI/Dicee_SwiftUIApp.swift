56db9642f6516eba56e6f6cbf88e7f4cf1690b69
dkutelov <dkutelov@digitals.bg>
2022-02-03 22:14:56 +0200

    Dicee swiftUI

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Dariy Kutelov on 3.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
